9b470bcd542c0d97f71ed1e32fb15ca072af29a8
Ben Parisot <me@benparisot.com>
2019-04-05 16:03:34 -0700

    transformers

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -6,7 +6,7 @@ describe('bitmap file transformer', () => {
 
     let buffer = null;
     beforeEach(() => {
-        
+
         // TODO: file read sync './test/test-bitmap.bmp' into buffer variable
     });
 
