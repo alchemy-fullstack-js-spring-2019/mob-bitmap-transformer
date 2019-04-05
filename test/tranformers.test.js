00544ffc3151e785638e7cc36bc601207cf48185
Anna Ridenour <annamridenour@gmail.com>
2019-04-05 16:55:22 -0700

    added third transformer

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -1,5 +1,6 @@
 const invert = require('../lib/invert-transformer');
 const grayscale = require('../lib/grayscale-transformer');
+const luminous = require('../lib/luminous-transformer');
 
 
 describe('transformers', () => {
@@ -32,7 +33,21 @@ describe('transformers', () => {
             g: 113,
             b: 113
         });
-    });
 
+    });
+    
+    it('luminous', () => {
+        const transformed = luminous({
+            r: 34,
+            g: 100,
+            b: 205
+        });
+
+        expect(transformed).toEqual({
+            r: 93.49000000000001,
+            g: 93.49000000000001,
+            b: 93.49000000000001
+        });
+    });
     // TODO: add a third transformer (you'll need to add the module and require!) and test
 });
