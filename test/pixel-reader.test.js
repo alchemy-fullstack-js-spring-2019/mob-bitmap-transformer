6a35e851b39014f16cc07b7f6064907fa5015a7a
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-08 15:11:48 -0700

    Cleaned up whitespace

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -7,23 +7,11 @@ describe('Pixel Reader', () => {
 
     const colors = [];
 
-    // TODO: subscribe to reader "color" event and push into `colors` array.
-    // A "color" object should look like:
-    // {
-    //     offset: <offset from the start of buffer passed to PixelReader>,
-    //     r: <red color value>,
-    //     g: <green color value>,
-    //     b: <blue color value>,
-    // }
     reader.on('color', pixel => {
       colors.push(pixel);
     });
 
     reader.on('end', () => {
-      // write deepEqual assertion for colors versus the
-      // expected rgb color objects
-      // expect().toEqual()
-      // expect(colors).toEqual({});
       expect(colors).toEqual([
         {
           'colors': {
@@ -49,22 +37,16 @@ describe('Pixel Reader', () => {
           },
           'offset': 6,
         },
-
       ]);
-
-      // Don't forget to call done()!
       done();
     });
 
-    // Create a buffer with known data for your colors
-    const buffer = Buffer.alloc(3 * 3); // for three pixels
-    // TODO: fill buffer with byte values that match your
-    // expected test colors
+    const buffer = Buffer.alloc(3 * 3);
+  
     for(let i = 0; i < buffer.length; i++) {
       buffer.writeUInt8((25 * i), i);
     }
-    // Call read method with your buffer
+  
     reader.read(buffer);
   });
-
 });
