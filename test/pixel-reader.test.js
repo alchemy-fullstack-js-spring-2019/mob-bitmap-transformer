bccdad8e1b82e07d42bad0e0104b2bfdce9bb1c5
Laura Badgley <l.badgleyy@gmail.com>
2019-04-08 21:09:37 -0700

    cleaning up and working on pixel reader/bitmap transformer

--- test/pixel-reader.test.js
+++ test/pixel-reader.test.js
@@ -3,31 +3,19 @@ const PixelReader = require('../lib/pixel-reader');
 describe('Pixel Reader', () => {
 
   it('reads pixel from buffer', done => {
-    const reader = new PixelReader({ bitsPerPixel: 24 });
-
+    const reader = new PixelReader({ bitsPerPixel: 24, pixelOffset: 10 });
     const colors = [];
 
-    reader.on('color', (color) => {
+    reader.on('color', color => {
       colors.push(color);
     });
-    // TODO: subscribe to reader "color" event and push into `colors` array.
-    // A "color" object should look like:
-    // {
-    //     offset: <offset from the start of buffer passed to PixelReader>,
-    //     r: <red color value>,
-    //     g: <green color value>,
-    //     b: <blue color value>,
-    // }
 
     reader.on('end', () => {
-      // write deepEqual assertion for colors versus the
-      // expect().toEqual()
-      // expected rgb color objects
       expect(colors).toHaveLength(3);
       expect(colors[0]).toEqual({
         r: 255,
-        g: 255,
-        b: 255,
+        g: 00,
+        b: 00,
         offset: 0
       });
       expect(colors[1]).toEqual({
@@ -45,25 +33,17 @@ describe('Pixel Reader', () => {
       done();
     });
 
-    // Create a buffer with known data for your colors
     const buffer = Buffer.alloc(9); 
-    //pixel 1
     buffer.writeUInt8(0xFF, 0);
-    buffer.writeUInt8(0xFF, 1);
-    buffer.writeUInt8(0xFF, 2);
-    //pixel 2
+    buffer.writeUInt8(0x00, 1);
+    buffer.writeUInt8(0x00, 2);
     buffer.writeUInt8(0x00, 3);
-    buffer.writeUInt8(0x00, 4);
+    buffer.writeUInt8(0xFF, 4);
     buffer.writeUInt8(0x00, 5);
-    //pixel 3
-    buffer.writeUInt8(0x50, 6);
-    buffer.writeUInt8(0x50, 7);
-    buffer.writeUInt8(0x50, 8);
-    // for three pixels
-    // TODO: fill buffer with byte values that match your
-    // expected test colors
+    buffer.writeUInt8(0x00, 6);
+    buffer.writeUInt8(0x00, 7);
+    buffer.writeUInt8(0xFF, 8);
 
-    // Call read method with your buffer
     reader.read(buffer);
   });
 
